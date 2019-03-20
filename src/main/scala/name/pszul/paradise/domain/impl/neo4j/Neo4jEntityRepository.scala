892b3639f2681afbbd75c30c41efec083abd9d37
Piotr Szul <piotr.szul@csiro.au>
2019-03-21 08:50:30 +1000

    Added logging in cypher queries

--- src/main/scala/name/pszul/paradise/domain/impl/neo4j/Neo4jEntityRepository.scala
+++ src/main/scala/name/pszul/paradise/domain/impl/neo4j/Neo4jEntityRepository.scala
@@ -12,11 +12,29 @@ import org.neo4j.driver.v1.AuthTokens
 import java.io.Closeable
 import name.pszul.paradise.domain.Path
 import name.pszul.paradise.domain.impl.neo4j.Mapper._
+import org.slf4j.LoggerFactory
 
 /**
  * Neo4j/Cypher implementation of EntityRepository
  */
 class Neo4jEntityRepository(driver: Driver) extends EntityRepository with Closeable {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
+  /**
+   * Runs a cypher query returing a single result.
+   * The query can containt variables that are subsituited with values from params, e.g.:
+   *
+   * `MATCH (n) WHERE ID(n) = $id`
+   *
+   * Fails with a runtime exception if query returns more than one result.
+   *
+   * @param cypherQuery cyher query to run
+   * @param params the map of with query parameters
+   * @mapper the function to map the query result to the expected type
+   *
+   * @return Some(T) if query returned a single result of None if no result was found.
+   */
 
   def querySingle[T](cypherQuery: String, params: Map[String, Any], mapper: Value => T): Option[T] = {
     // scalastyle:off
@@ -24,6 +42,7 @@ class Neo4jEntityRepository(driver: Driver) extends EntityRepository with Closea
     // scalastyle:on
     try {
       session = driver.session()
+      logger.info("Running cypher query: `{}` with parms: {}", cypherQuery, params, None)
       val statementResult = session.run(cypherQuery, params.mapValues(_.asInstanceOf[Object]).asJava)
       if (statementResult.hasNext()) {
         val record = statementResult.single()
